6752565655524cbad34bc9023a230fd190820a65
TharunikaAshokKumar <ta1101@rit.edu>
2024-07-13 19:23:10 -0400

    Update ai_model.py

--- src/ai_model.py
+++ src/ai_model.py
@@ -1,1 +1,12 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+def predict_stock_level(data):
+    df = pd.DataFrame(data)
+    model = LinearRegression()
+    X = df[['current_stock']]
+    y = df['sales']
+    model.fit(X, y)
+    df['predicted_stock'] = model.predict(X)
+    return df['predicted_stock'].tolist()
 
